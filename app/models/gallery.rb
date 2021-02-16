55e4463264896f1fdf8e3b64ce98504c3cb672f3
maiche <m.otsuka2796@gmail.com>
2021-02-16 19:44:55 +0900

    Galleryモデルのバリデーションとアソシエーションの設定

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,2 +1,7 @@
 class Gallery < ApplicationRecord
+  with_options presence: true do
+    validates :title, :description
+  end
+  
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :galleries
 end
