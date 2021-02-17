e93a12bedf89629fce88b90dd4904797cd35c2ee
maiche <m.otsuka2796@gmail.com>
2021-02-17 17:15:06 +0900

    createアクションの作成

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -7,5 +7,17 @@ class GalleriesController < ApplicationController
   end
 
   def create
+    @gallery = Gallery.new(gallery_params)
+    if @gallery.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def gallery_params
+    params.require(:gallery).permit(:title, :top_image, :description, artworks: []).merge(user_id: current_user.id)
   end
 end
