5effb986e92e274504de3420443834cdad59c39b
maiche <m.otsuka2796@gmail.com>
2021-02-17 21:46:38 +0900

    ユーザーによる表示の切り替え

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,4 +1,8 @@
 class GalleriesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_gallery, only: [:edit, :update, :show, :destroy]
+  before_action :move_to_root, only: [:edit, :update, :destroy]
+
   def index
     @galleries = Gallery.all.order('created_at DESC')
   end
@@ -17,11 +21,9 @@ class GalleriesController < ApplicationController
   end
 
   def edit
-    @gallery = Gallery.find(params[:id])
   end
 
   def update
-    @gallery = Gallery.find(params[:id])
     if @gallery.update(gallery_params)
       redirect_to root_path
     else
@@ -30,11 +32,9 @@ class GalleriesController < ApplicationController
   end
 
   def show
-    @gallery = Gallery.find(params[:id])
   end
 
   def destroy
-    @gallery = Gallery.find(params[:id])
     @gallery.destroy
     redirect_to root_path
   end
@@ -43,5 +43,13 @@ class GalleriesController < ApplicationController
 
   def gallery_params
     params.require(:gallery).permit(:title, :top_image, :description, artworks: []).merge(user_id: current_user.id)
+  end
+
+  def set_gallery
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def move_to_root
+    redirect_to root_path if current_user.id != @gallery.user_id
   end
 end
