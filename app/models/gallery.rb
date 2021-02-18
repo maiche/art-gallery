403784124d60c8fdc9d91012803da3421eab574c
maiche <m.otsuka2796@gmail.com>
2021-02-18 12:42:43 +0900

    複数テーブルに分けて保存

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,10 @@
 class Gallery < ApplicationRecord
-  with_options presence: true do
-    validates :title, :description, :artworks
-  end
+  # with_options presence: true do
+  #   validates :title, :description, :artworks
+  # end
   
   belongs_to :user
   has_one_attached :top_image
-  has_many_attached :artworks
+  # has_many_attached :artworks
   has_one :room
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
   belongs_to :gallery
+  has_many_attached :artworks
 end
