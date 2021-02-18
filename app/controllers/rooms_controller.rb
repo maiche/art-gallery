4cb11de13fef07d77cd905da9052ea636729700b
maiche <m.otsuka2796@gmail.com>
2021-02-18 22:25:13 +0900

    room編集ページの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,14 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @gallery = Gallery.find(params[:gallery_id])
     @room = @gallery.rooms[0]
     @artworks = @room.artworks
+  end
+
+  def edit
+    @gallery = Gallery.find(params[:gallery_id])
+    redirect_to root_path if current_user.id != @gallery.user_id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'galleries#index'
   resources :galleries do
-    resources :rooms, only: :index
+    resources :rooms, only: [:index, :edit]
   end
   resources :users, only: :show
 end
