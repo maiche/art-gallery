f566304010ec3f425b74037735c56801ea8510be
maiche <m.otsuka2796@gmail.com>
2021-02-19 19:29:03 +0900

    jsファイルの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('./preview')
-require('./position')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
