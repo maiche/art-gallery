d6f4a631128f6a1fe3dbc165701dadda81805a9a
maiche <m.otsuka2796@gmail.com>
2021-02-26 11:39:36 +0900

    編集権限の修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,15 +1,14 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_gallery, only: [:index, :edit, :update]
+  before_action :move_to_root, only: [:edit, :update]
 
   def index
-    @gallery = Gallery.find(params[:gallery_id])
     @room = @gallery.rooms[0]
     @artworks = @room.artworks
   end
 
   def edit
-    @gallery = Gallery.find(params[:gallery_id])
-    redirect_to root_path if current_user.id != @gallery.user_id
     @room = Room.find(params[:id])
     @artworks = @room.artworks
   end
@@ -18,5 +17,15 @@ class RoomsController < ApplicationController
     artwork = Artwork.find(params[:artwork_id])
     artwork.update(vertical: params[:vertical], horizontal: params[:horizontal], size: params[:size])
     render json:{ artwork: artwork }
+  end
+
+  private
+
+  def set_gallery
+    @gallery = Gallery.find(params[:gallery_id])
+  end
+
+  def move_to_root
+    redirect_to root_path if current_user.id != @gallery.user_id
   end
 end
